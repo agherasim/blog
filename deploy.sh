25c38e61fc91f7b856c0b4ef9c44b3b65e1513be
Andrei Gherasim <andrei.gherasim@fashiondays.com>
2014-09-29 21:31:05 +0300

    Deploying release

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo 'Deploying blog...'
 
 grunt release
 git clone ssh://5426e81d4382ec9a56000590@blog-agherasim.rhcloud.com/~/git/blog.git/ .blog
-mv ./dist/releases/*.zip .blog
+mv ./dist/release/*.zip .blog/release.zip
 git add -A
 git commit -m "Deploying release"
 #git push origin master
