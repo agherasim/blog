9c5a5778f599c2b1686c1913b1d93e7f5205eeea
Andrei Gherasim <andrei.gherasim@fashiondays.com>
2014-09-29 22:14:56 +0300

    adding config for openshift, updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo 'Deploying blog...'
 
+BUILD_DIR=/tmp/.andreigherasim.net
+rm -rf $BUILD_DIR
+
 grunt release
-git clone ssh://5426e81d4382ec9a56000590@blog-agherasim.rhcloud.com/~/git/blog.git/ .blog
-mv ./dist/release/*.zip .blog/release.zip
+git clone ssh://5426e81d4382ec9a56000590@blog-agherasim.rhcloud.com/~/git/blog.git/ $BUILD_DIR
+mv .dist/release/*.zip $BUILD_DIR/release.zip
+
+cd $BUILD_DIR
 git add -A
 git commit -m "Deploying release"
-#git push origin master
+git push origin master
